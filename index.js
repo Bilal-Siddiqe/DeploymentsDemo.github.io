74fc90222eee8221c78ebf9e58f3598ec893dbc9
Husnain-Mushtaq <hasnainscl9111@gmail.com>
2023-04-04 23:31:08 +0500

    Index File Attached

---
+++ index.js
@@ -0,0 +1,16 @@
+// let num1 = prompt("Please Enter 1st Number");
+// let num2 = prompt("Please Enter 2nd Number");
+
+// let add = num1 + num2 ;
+
+// document.write(add);
+
+
+let num1 = parseFloat(prompt("Enter Number"));
+let num2 = parseFloat(prompt("Enter Number"));
+  
+
+
+document.write(num1+num2)
+
+
